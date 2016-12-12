84d6aa9ad1718cd5bd465b56506c214f3e6ccec1
IBIS, Esat <esatibis@expertech.net>
2016-12-12 14:35:42 -0500

    added login with google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { AngularFire } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Please Login!';
+  constructor(public af: AngularFire) {
+    this.af.auth.subscribe(auth => console.log(auth));
+  }
+  login() {
+    this.af.auth.login();
+  }
+  logout() {
+     this.af.auth.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+// Firebase config file
+const firebaseConfig = {
+    apiKey: "AIzaSyD1cAWGvM6v5wFLcbJEmvA5xoJPSDXltGY",
+    authDomain: "comp2112-chat.firebaseapp.com",
+    databaseURL: "https://comp2112-chat.firebaseio.com",
+    storageBucket: "comp2112-chat.appspot.com",
+    messagingSenderId: "1090716101207"
+  };
+
+  const myFirebaseAuthConfig = {
+    provider: AuthProviders.Google,
+    method: AuthMethods.Redirect
+  }
 
 @NgModule({
   declarations: [
@@ -12,7 +27,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
